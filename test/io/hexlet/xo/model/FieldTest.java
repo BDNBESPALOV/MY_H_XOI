6034cfbb17f643963f89a6a093dafc4103ca45d4
Dmitry <bdn.bespalov@gmail.com>
2019-05-14 14:04:22 +0300

    Point Already Occupied Exception created2

--- test/io/hexlet/xo/model/FieldTest.java
+++ test/io/hexlet/xo/model/FieldTest.java
@@ -1,5 +1,7 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exception.InvalidPointException;
+import io.hexlet.xo.model.exception.PointAlreadyOccupiedException;
 import org.junit.Test;
 
 import java.awt.*;
@@ -9,19 +11,76 @@ import static org.junit.Assert.*;
 public class FieldTest {
 
     @Test
-    public void getSize() {
+    public void testGetSize() {
         final Field field=new Field();
         assertEquals(3,field.getSize());
     }
 
     @Test
-    public void setFigure() {
+    public void testSetFigure() {
         final Field field=new Field();
         final Point point=new Point(1,1);
         final Figure inputFigure= Figure.X;
-        field.setFigure(point,inputFigure);
-        final Figure actualFigure=field.getFigure(point);
+        try{
+            field.setFigure(point,inputFigure);
+            final Figure actualFigure=field.getFigure(point);
+            assertEquals(inputFigure,actualFigure);
+        }catch (PointAlreadyOccupiedException e){}
+        catch (InvalidPointException e){}
 
-        assertEquals(inputFigure,actualFigure);
+
+
+    }
+    @Test
+    public void testGetFigureIsNotSet() {
+        final Field field=new Field();
+        final Point point=new Point(1,1);
+
+        try{
+
+            final Figure actualFigure=field.getFigure(point);
+             assertNull(actualFigure);
+        }
+        catch (InvalidPointException e){}
+    }
+    @Test
+    public void testGetFigureXThenZero() {
+        final Field field=new Field();
+        final Point point=new Point(0,-1);
+
+        try{
+
+            field.getFigure(point);
+            fail();
+        }
+        catch (InvalidPointException e){
+
+        }
+    }
+    @Test
+    public void testGetFigureXThenSize() {
+        final Field field=new Field();
+        final Point point=new Point(field.getSize()+1,0);
+
+        try{
+            field.getFigure(point);
+            fail();
+        }
+        catch (InvalidPointException e){
+
+        }
+    }
+    public void testGetFigureYThenSize() {
+        final Field field=new Field();
+        final Point point=new Point(0,-1);
+
+        try{
+
+            field.getFigure(point);
+            fail();
+        }
+        catch (InvalidPointException e){
+
+        }
     }
 }
