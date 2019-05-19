3930eec7b490393c793504710b4f861d0eec3f68
Дмитрий <bdn.bespalov@gmail.com>
2019-05-19 23:08:40 +0300

    create test CurrentMoveController

---
+++ test/io/hexlet/xo/controller/CurrentMoveConrollerTest.java
@@ -0,0 +1,25 @@
+package io.hexlet.xo.controller;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exception.InvalidPointException;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class CurrentMoveConrollerTest {
+
+    @Test
+    public void testMoveController() throws InvalidPointException {
+        CurrentMoveConroller currentMoveConroller =new CurrentMoveConroller();
+        Field field=new Field(3);
+        field.setFigure(new Point(0,0), Figure.X);
+        field.setFigure(new Point(0,1), Figure.O);
+        field.setFigure(new Point(0,2), Figure.X);
+        assertEquals(Figure.O,currentMoveConroller.moveController(field));
+
+
+    }
+}
