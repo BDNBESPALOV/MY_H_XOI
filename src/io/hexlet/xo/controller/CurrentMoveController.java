fc1f986f07e3a73c6675636bdf9f30842ac8bf82
Dmitry <bdn.bespalov@gmail.com>
2019-05-20 18:11:31 +0300

    111

--- src/io/hexlet/xo/controller/CurrentMoveController.java
+++ src/io/hexlet/xo/controller/CurrentMoveController.java
@@ -6,15 +6,19 @@ import io.hexlet.xo.model.exception.InvalidPointException;
 
 import java.awt.*;
 
-public class CurrentMoveConroller {
-    public Figure moveController(final Field field) throws InvalidPointException {
+public class CurrentMoveController {
+    public Figure moveController(final Field field)  {
         int countFigure=0;
 
         for(int y=0;y<field.getSize();y++){
         for(int i=0;i<field.getSize();i++){
-if(field.getFigure(new Point(y,i)) != null){
-    countFigure++;
-}
+            try {
+                if(field.getFigure(new Point(y,i)) != null){
+                    countFigure++;
+                }
+            } catch (InvalidPointException e) {
+                e.printStackTrace();
+            }
         }}
         if(countFigure == field.getSize()*field.getSize()){
             return null;
